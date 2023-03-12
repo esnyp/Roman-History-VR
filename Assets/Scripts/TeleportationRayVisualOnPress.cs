cf61bfc8dc720662ef0685c2e2d03b48769177a9
esnyp <1915345@brunel.ac.uk>
2023-03-12 06:11:45 +0000

    UI
    -added ui (no functionality yet)
    -fixed hand ray bugs

--- Assets/Scripts/TeleportationRayVisualOnPress.cs
+++ Assets/Scripts/TeleportationRayVisualOnPress.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.XR.Interaction.Toolkit;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class TeleportationRayVisualOnPress : MonoBehaviour
 {
@@ -10,13 +12,20 @@ public class TeleportationRayVisualOnPress : MonoBehaviour
 
     public InputActionProperty leftActivate;
     public InputActionProperty rightActivate;
-    
 
+    public InputActionProperty leftCancel;
+    public InputActionProperty rightCancel;
+
+    public XRRayInteractor leftRay;
+    public XRRayInteractor rightRay;
 
     // Update is called once per frame
     void Update()
     {
-        leftTeleportation.SetActive(leftActivate.action.ReadValue<float>() > 0.1f);
-        rightTeleportation.SetActive(rightActivate.action.ReadValue<float>() > 0.1f);
+        bool isLeftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftNumber, out bool leftValid);
+        bool isRightRayHovering = leftRay.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightNumber, out bool rightValid);
+
+        leftTeleportation.SetActive(!isLeftRayHovering && leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
+        rightTeleportation.SetActive(!isRightRayHovering && rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
     }
 }
